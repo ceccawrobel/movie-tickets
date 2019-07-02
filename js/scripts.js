f341451df513b881220494073252dbd1f17f96ec
carrie schmid and cecca wrobel <carrie+cecca@>
2019-07-01 20:50:41 -0700

    include first steps on price prototype

--- js/scripts.js
+++ js/scripts.js
@@ -4,26 +4,29 @@ function Ticket() {
   this.age = age;
   this.time = time;
   this.movieType = movieType;
-  this.price = price;
 }
 
 // Ticket.prototype.price = function() {
 //   this.price = 10
 // }
 
-var youthEarlyFirst = new Ticket("youth", "early", "first", 6)
-var youthEarlySecond = new Ticket("youth", "early", "second", 5)
-var youthLateFirst = new Ticket("youth", "late", "first", 5)
-var youthLateSecond = new Ticket("youth", "late", "second", 5)
-var adultEarlyFirst = new Ticket("adult", "early", "first", 5)
-var adultEarlySecond = new Ticket("adult", "early", "second", 5)
-var adultLateFirst = new Ticket("adult", "late", "first", 10)
-var adultLateSecond = new Ticket("adult", "late", "second", 8)
+//do this prototype^ instead of all these objects-->
+//..and finish it by writing branching statements to calculate "minus 2 for youth" etc
+
+// var youthEarlyFirst = new Ticket("youth", "early", "first")
+// var youthEarlySecond = new Ticket("youth", "early", "second")
+// var youthLateFirst = new Ticket("youth", "late", "first")
+// var youthLateSecond = new Ticket("youth", "late", "second")
+// var adultEarlyFirst = new Ticket("adult", "early", "first")
+// var adultEarlySecond = new Ticket("adult", "early", "second")
+// var adultLateFirst = new Ticket("adult", "late", "first")
+// var adultLateSecond = new Ticket("adult", "late", "second")
 
 
 
 // user interface logic
 
 $(document).ready(function() {
-
+var userAge =
+var userTicket = new Ticket(userAge, userTime, userMovieType)
 })
