74faf2161004b28826e0446624262e96533a026f
carrie schmid and cecca wrobel <carrie@>
2019-07-03 20:33:56 -0700

    add checkbox menu for snacks and js to add up snack value total

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ Ticket.prototype.price = function() {
     userPrice -= 2
   }
 
-  alert(userPrice)
+  return userPrice
 }
 
 
@@ -38,7 +38,34 @@ $(document).ready(function() {
     var userTime = $("#userTime").val();
     var userMovieType = $("#userMovieType").val();
     var userTicket = new Ticket(userAge, userTime, userMovieType);
-    userTicket.price()
+
+    var result = userTicket.price();
+    $("#userPriceResult").show();
+    $("#userPriceResult").text("The ticket price will be $" + result + ".00")
   })
+
+  $("#userSnacks").submit(function(event) {
+    event.preventDefault();
+    var snackList = [];
+    var snackTotal = 0;
+
+    $("input:checkbox[name=snack-option]:checked").each(function() {
+      var eachSnack = parseInt($(this).val());
+      snackList.push(eachSnack);
+    })
+    console.log(snackList);
+
+    snackList.forEach(function(snackItem) {
+      snackTotal = snackTotal + snackItem
+    })
+    console.log(snackTotal);
+
+    $("#userSnacksResult").show();
+    $("#userSnacksResult").text("The snack total will be $" + snackTotal + ".00");
+
+
+  })
+
+
 
 })
